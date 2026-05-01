46928cfbd3c7e3d2bffb88db159f7b6457ef6953
PRINCE CEEJAY <ceejaydroidprince@gmail.com>
2026-05-01 16:09:47 +0100

    completed CRUD in productAPI

--- src/features/products/productApi.ts
+++ src/features/products/productApi.ts
@@ -2,17 +2,45 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 
 export const productApi = createApi({
     reducerPath: 'productApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://dummyjson.com'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'https://dummyjson.com/'}),
     endpoints: (builder) => ({
         getProducts: builder.query({
-            query: ()=> '/products'
+            query: ()=> 'products'
         }),
         
         getProductById: builder.query({
-            query: (id) => `/products/${id}`
+            query: (id) => `products/${id}`
+        }), 
+
+        addProduct: builder.mutation({
+            query: (product) => ({
+                url: 'products/add',
+                method: 'POST',
+                body: product,
+            })
+        }),
+
+        removeProduct: builder.mutation({
+            query: (productId) =>({
+                url: `products/${productId}`,
+                method: 'DELETE'
+            })
+        }),
+
+        updateProduct: builder.mutation({
+            query: (action) => ({
+                url: `products/${action.paylod.id}`,
+                method: 'PUT',
+                body: action.payload 
+            })
         })
     }),
 
 })
 
-export const {useGetProductsQuery, useGetProductByIdQuery} = productApi
+export const {
+    useGetProductsQuery, 
+    useGetProductByIdQuery, 
+    useAddProductMutation,
+    useUpdateProductMutation,
+} = productApi
